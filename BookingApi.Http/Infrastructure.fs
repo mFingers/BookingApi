46ce2002ae4dc12e2c95a042c1024edb30b3d377
Michael Falanga <mfalanga@manifestcorp.com>
2016-03-08 16:50:27 -0500

    Create an agent for handling incoming reservation requests

--- BookingApi.Http/Infrastructure.fs
+++ BookingApi.Http/Infrastructure.fs
@@ -4,8 +4,30 @@ open System
 open System.Web.Http
 open System.Web.Http.Dispatcher
 open Controllers
+open Reservations
+
+type Agent<'T> = Microsoft.FSharp.Control.MailboxProcessor<'T>
 
 type CompositionRoot() =
+    let seatingCapacity = 10
+    let reservations = System.Collections.Concurrent.ConcurrentBag<Envelope<Reservation>>()
+
+    let agent = new Agent<Envelope<MakeReservation>>(fun inbox ->
+        let rec loop () =
+            async {
+                let! cmd = inbox.Receive()
+                let rs = reservations |> ToReservations
+                let handle = Handle seatingCapacity rs
+                let newReservations = handle cmd
+                match newReservations with
+                | Some (r) -> reservations.Add r
+                | None     -> ()
+                return! loop()
+            }
+        loop())
+
+    do agent.Start()
+
     interface IHttpControllerActivator with
         member this.Create(request, controllerDescriptor, controllerType) =
             if controllerType = typeof<HomeController> then
