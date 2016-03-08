13fcd93dfc91ca9c0255fd5125b4833cd2d31def
Michael Falanga <mfalanga@manifestcorp.com>
2016-03-08 16:40:24 -0500

    Hook into WebAPI controller creation so that the business model can subscribe to the controller

--- BookingApi.Http/Infrastructure.fs
+++ BookingApi.Http/Infrastructure.fs
@@ -1,8 +1,29 @@
 ﻿module BookingApi.Http.Infrastructure
 
+open System
 open System.Web.Http
+open System.Web.Http.Dispatcher
+open Controllers
+
+type CompositionRoot() =
+    interface IHttpControllerActivator with
+        member this.Create(request, controllerDescriptor, controllerType) =
+            if controllerType = typeof<HomeController> then
+                new HomeController() :> IHttpController
+            elif controllerType = typeof<ReservationsController> then
+                let c = new ReservationsController()
+                c :> IHttpController
+            else
+                raise
+                <| ArgumentException(
+                    sprintf "Unknown controller type requested: %O" controllerType, "controllerType")
 
 type HttpRouteDefaults = { Controller:string; Id:obj }
+
+let ConfigureServices (config:HttpConfiguration) =
+    config.Services.Replace(
+        typeof<IHttpControllerActivator>,
+        CompositionRoot())
 
 let ConfigureRoutes (config:HttpConfiguration) =
     config.Routes.MapHttpRoute(
@@ -16,4 +37,5 @@ let ConfigureFormatting (config:HttpConfiguration) =
 
 let Configure config =
     ConfigureRoutes config
+    ConfigureServices config
     ConfigureFormatting config
