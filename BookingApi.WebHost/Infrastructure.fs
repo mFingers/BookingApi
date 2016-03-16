1868939a006bf0b8ce12fe47f776a06800e23c05
Michael Falanga <mfalanga@manifestcorp.com>
2016-03-15 21:14:10 -0400

    Create types for using Azure storage

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -12,6 +12,8 @@ open System.Reactive
 open FSharp.Control.Reactive
 open FSharp.Control.Reactive.Observable
 open Newtonsoft.Json
+open Microsoft.WindowsAzure.Storage.Blob
+open Microsoft.WindowsAzure.Storage
 
 type ReservationsInFiles(directory:DirectoryInfo) =
     let toReservation (f:FileInfo) =
@@ -110,6 +112,90 @@ type NotificationsInFiles(directory:DirectoryInfo) =
         member x.GetEnumerator() =
             (x :> seq<Envelope<BookingApi.Http.Notification>>).GetEnumerator() :> System.Collections.IEnumerator
 
+[<CLIMutable>]
+type StoredReservations = {
+    Reservations: Envelope<Reservation> array
+    AcceptedCommandIds: Guid array
+}
+
+type ReservationsInAzureBlobs(blobContainer: CloudBlobContainer) =
+    let toReservation (b: CloudBlockBlob) =
+        let json = b.DownloadText()
+        let sr = JsonConvert.DeserializeObject<StoredReservations> json
+        sr.Reservations
+
+    let toEnumerator (s: seq<'T>) = s.GetEnumerator()
+
+    let getId (d:DateTime) =
+        String.Join(
+            "/",
+            [
+                d.Year.ToString(),
+                d.Month.ToString(),
+                d.Day.ToString()
+            ])
+            |> sprintf "%s.json"
+
+    member this.GetAccessCondition date =
+        let id = date |> getId
+        let b = blobContainer.GetBlockBlobReference id
+        try
+            b.FetchAttributes()
+            b.Properties.ETag |> AccessCondition.GenerateIfMatchCondition
+        with
+        | :? StorageException as e when e.RequestInformation.HttpStatusCode = 404 ->
+                AccessCondition.GenerateIfNoneMatchCondition "*"
+
+    member this.Write (reservation: Envelope<Reservation>, commandId, condition) =
+        let id = reservation.Item.Date |> getId
+        let b = blobContainer.GetBlockBlobReference id
+        let inStore = 
+            try
+                let jsonInStore = b.DownloadText(accessCondition = condition)
+                JsonConvert.DeserializeObject<StoredReservations> jsonInStore
+            with
+            | :? StorageException as e when e.RequestInformation.HttpStatusCode = 404 ->
+                { Reservations = [||]; AcceptedCommandIds = [||] }
+
+        let updated =
+            {
+                Reservations =
+                    Array.append [| reservation |] inStore.Reservations
+                AcceptedCommandIds =
+                    Array.append [| commandId |] inStore.AcceptedCommandIds
+            }
+
+        let json = JsonConvert.SerializeObject updated
+        b.Properties.ContentType <- "application/json"
+        b.UploadText(json, accessCondition = condition)
+
+    interface IReservations with
+        member this.Between min max =
+            Dates.InitInfinite min
+            |> Seq.takeWhile (fun d -> d <= max)
+            |> Seq.map getId
+            |> Seq.map blobContainer.GetBlockBlobReference
+            |> Seq.filter (fun b -> b.Exists())
+            |> Seq.collect toReservation
+
+        member this.GetEnumerator() =
+            blobContainer.ListBlobs()
+            |> Seq.cast<CloudBlockBlob>
+            |> Seq.collect toReservation
+            |> toEnumerator
+
+        member this.GetEnumerator() =
+            (this :> seq<Envelope<Reservation>>).GetEnumerator() :> System.Collections.IEnumerator
+
+module AzureQ =
+    let enqueue (q:Queue.CloudQueue) msg =
+        let json = JsonConvert.SerializeObject msg
+        Queue.CloudQueueMessage(json) |> q.AddMessage
+
+    let dequeue (q:Queue.CloudQueue) =
+        match q.GetMessage() with
+        | null -> None
+        | msg -> Some(msg)
 
 type Agent<'T> = Microsoft.FSharp.Control.MailboxProcessor<'T>
 
